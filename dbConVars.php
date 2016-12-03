c41c97832d754a9aed8baa07e5089d83fa732591
alxngyn <alexlnguyen@gmail.com>
2016-12-03 01:57:40 -0800

    first draft users.php

---
+++ dbConVars.php
@@ -0,0 +1,6 @@
+<?php
+    $DB_HOST = "oniddb.cws.oregonstate.edu";
+    $DB_NAME = "nguyalex-db";
+    $DB_USER = "nguyalex-db";
+    $DB_PASSWORD = "sf4PChMUktJ2TsEf";
+?>
---
+++ users.php
@@ -0,0 +1,28 @@
+<?php
+require_once('dbConVars.php');
+
+$servername = $DB_HOST;
+$username = $DB_USER;
+$password = $DB_PASSWORD;
+$dbname = $DB_HOST;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
